d039f98b1e08c961f37d51e4da91826e52a9d2f2
Brandon Olivares <programmer2188@gmail.com>
2025-10-30 01:06:27 -0400

    chore: Configure strict TypeScript linting and compiler options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,24 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import tseslint from 'typescript-eslint'
+import vueparser from 'vue-eslint-parser'
 
 export default withNuxt(
-  // Your custom configs here
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parser: vueparser,
+      parserOptions: {
+        parser: tseslint.parser,
+        sourceType: 'module',
+        projectService: {
+          allowDefaultProject: ['eslint.config.mjs']
+        }
+      }
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': ['error']
+    }
+  }
 )
