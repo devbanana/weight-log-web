a49c594f512c5f8c04c1380e58933f20e8cee550
Brandon Olivares <programmer2188@gmail.com>
2025-10-31 03:32:36 -0400

    feat: Add logout page

---
+++ app/pages/logout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+await useAPI('/auth/logout', { method: 'POST' })
+
+await navigateTo('/')
+</script>
+
+<template>
+  <UContainer class="py-12">
+    <p class="text-center">
+      Logging out...
+    </p>
+  </UContainer>
+</template>
