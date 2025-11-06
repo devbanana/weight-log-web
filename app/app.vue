d0762bb543c75c3dbc7c4a55d5ac43c5353289d1
Brandon Olivares <programmer2188@gmail.com>
2025-11-06 01:36:49 -0500

    chore: import AppLogo and TemplateMenu components

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { useHead, useSeoMeta } from '#app'
 
+import AppLogo from '~/components/AppLogo.vue'
+import TemplateMenu from '~/components/TemplateMenu.vue'
+
 useHead({
   meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
   link: [{ rel: 'icon', href: '/favicon.ico' }],
